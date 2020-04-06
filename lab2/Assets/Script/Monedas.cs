d608600f4d5f1b9a1aa1366427c189f3702fce7e
lauratamath <laurylevi@hotmail.com>
2020-04-05 22:16:21 -0600

    Agregue para que con la tecla space o enter aparezca la pelota

--- lab2/Assets/Script/Monedas.cs
+++ lab2/Assets/Script/Monedas.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Monedas : MonoBehaviour
 {
     public float veloAngular = 0;
+    public GameObject prefab;
+    private GameObject nuevo;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,5 +17,9 @@ public class Monedas : MonoBehaviour
     void Update()
     {
         transform.Rotate(new Vector3(veloAngular, 0, 0) * Time.deltaTime);
+        if (Input.GetButtonDown("Submit") && prefab && !nuevo)
+        {
+            nuevo = Instantiate<GameObject>(prefab, new Vector3(-3.3f, 1.17f, -2.57f), Quaternion.identity);
+        }
     }
 }
